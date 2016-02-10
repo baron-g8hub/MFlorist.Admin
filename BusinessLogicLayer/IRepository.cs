1d31f2797439d6f13602e0a63ce3c6e0ccfe8e4f
baron-g8hub <lugtubaron@gmail.com>
2016-02-10 17:16:14 +0800

    Final project with working repository pattern.

---
+++ BusinessLogicLayer/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    /// <summary>
+    /// Author  : Baron Lugtu (Maynooth Florist Item)
+    /// Date    : February 9, 2016 
+    /// 
+    /// An interface for the data store. This will be implemented based on the scenario using Dependency Injection
+    /// 
+    /// Scenario 1: Unit Tests - No need actual connection to datastore or database
+    /// Scenario 2: Actual usage of Database or datastore
+    /// </summary>
+    public interface IRepository
+    {
+        IQueryable<MaynoothFloristItem> GetItemList();
+        IList<MaynoothFloristItem> GetFlowersList();
+
+        IEnumerable<MaynoothFloristItem> Get();
+
+        bool SaveItem(MaynoothFloristItem item);
+        bool IsItemExist(decimal FlowerId);
+    }
+}
---
+++ BusinessLogicLayer/MaynoothFloristItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BusinessLogicLayer
+{
+    /// <summary>
+    /// Author  : Baron Lugtu (Maynooth Florist)
+    /// Date    : February 9, 2016
+    /// 
+    /// BLL - Maynooth Florist Item domain object
+    /// </summary>
+    [Serializable]
+    public class MaynoothFloristItem
+    {
+        
+        public decimal FlowerId { get; set; }
+
+        
+        public string FlowerName { get; set; }
+
+       
+        public decimal? FlowerPrice { get; set; }
+        
+
+        public string FlowerDescription { get; set; }
+
+       
+        public string FlowerImageUrl { get; set; }
+        
+        
+        public IRepository Repository { get; set; }
+
+        public IQueryable<MaynoothFloristItem> GetItemList()
+        {
+            return Repository.GetItemList();
+        }
+
+        public IEnumerable<MaynoothFloristItem> Get()
+        {
+            return Repository.Get();
+        }
+             
+        public IList<MaynoothFloristItem> GetFlowersList()
+        {
+            return Repository.GetFlowersList();
+        }
+    }
+}
